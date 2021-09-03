5539cb559160d1cb547e0e11f4899744318c5b63
Christian Ensel <ensel@quantentunnel.de>
2021-09-03 13:51:22 +0200

    added getPos, setPos, die, isWhite corrected bug in baseValue

--- src/main/java/de/ensel/tideeval/ChessPiece.java
+++ src/main/java/de/ensel/tideeval/ChessPiece.java
@@ -5,6 +5,8 @@
 
 package de.ensel.tideeval;
 
+import javax.swing.*;
+
 import static de.ensel.tideeval.ChessBasics.*;
 import static de.ensel.tideeval.ChessBoard.MAX_INTERESTING_NROF_HOPS;
 
@@ -12,11 +14,22 @@ public class ChessPiece {
     final ChessBoard myChessBoard;
     private final int myPceTypeNr;
     private final int myPceID;
+    private int myPos;
 
-    ChessPiece(ChessBoard myChessBoard, int pceTypeNr, int pceID) {
+    public int getPieceTypeNr() {
+        return myPceTypeNr;
+    }
+
+    public int getPieceID() {
+        return myPceID;
+    }
+
+
+    ChessPiece(ChessBoard myChessBoard, int pceTypeNr, int pceID, int pcePos) {
         this.myChessBoard = myChessBoard;
         myPceTypeNr = pceTypeNr;
         myPceID = pceID;
+        myPos = pcePos;
     }
 
     @Override
@@ -29,6 +42,8 @@ public class ChessPiece {
     }
 
     int getBaseValue() {
+        if (isPieceTypeNrBlack(myPceTypeNr))
+            return -getPieceBaseValue(myPceTypeNr);
         return getPieceBaseValue(myPceTypeNr);
     }
 
@@ -44,9 +59,29 @@ public class ChessPiece {
         // and b) opponent blocking the way (but which also "pins" him there to keep it up)
         int[] mobilityCountForHops = new int[MAX_INTERESTING_NROF_HOPS];
         for( Square sq : myChessBoard.getBoardSquares() ) {
-            mobilityCountForHops[sq.getDistanceToPieceID(myPceID)]++;
+            int distance = sq.getDistanceToPieceID(myPceID);
+            if (distance!=0 && distance<=MAX_INTERESTING_NROF_HOPS)
+                mobilityCountForHops[distance-1]++;
         }
         return mobilityCountForHops;
     }
 
+    public int getPos() {
+        return myPos;
+    }
+
+    public void setPos(int pos) {
+        myPos = pos;
+    }
+
+    /**
+     * die() piece is EOL - clean up
+     */
+    public void die() {
+        // actually nothing to clean up here...
+    }
+
+    public boolean isWhite() {
+        return ChessBasics.isPieceTypeNrWhite(myPceTypeNr);
+    }
 }
